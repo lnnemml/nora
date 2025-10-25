39ec046f5fd6bc25e472b4015c7d2fc641498d1b
lnnemml <lnnemml@gmail.com>
2025-10-25 12:44:31 +0300

    Fix formatting and add unoptimized option in config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
     formats: ['image/avif', 'image/webp'],
+    unoptimized: false,
   },
   compress: true,
   poweredByHeader: false,
